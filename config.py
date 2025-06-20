3e9b97797d0cc4c50afb8362df64f56476d088b3
Sirojiddin0795 <sirojiddin0795@gmail.com>
2025-06-20 20:50:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = set(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+raw_ids = os.getenv("ADMIN_IDS", "")
+ADMIN_IDS = set()
+if raw_ids.strip():
+    ADMIN_IDS = set(map(int, raw_ids.split(",")))
